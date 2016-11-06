14ff7350d7e3bb654c786d24da88ef739628fc01
River Gillis <jrivergillis@gmail.com>
2016-11-06 09:41:54 -0600

    Implement checking filetype for html file

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 # returns a list of all of the links from a webpage
 from bs4 import BeautifulSoup, SoupStrainer
+from tld import get_tld
 import requests
 import re
 
@@ -18,7 +19,6 @@ def remove_anchor(link):
     return link.split("#")[0]
 
 def clean_link(base_url, dirty_link, root_url):
-    print("cleaning link", dirty_link, "with base", base_url)
     no_anchor = remove_anchor(dirty_link)
     if no_anchor.startswith('http://') or no_anchor.startswith('https://'):
         return no_anchor
@@ -31,15 +31,12 @@ def clean_link(base_url, dirty_link, root_url):
             no_anchor = no_anchor[3:]
             base_url = base_url.rsplit("/", 2)[0]
         elif no_anchor.startswith("/"): # root + extra
-            print("found a root call, root is", root_url)
-            print("returning", root_url + no_anchor[1:])
             return root_url + no_anchor[1:]
         else:
             base_url = base_url.rsplit("/", 1)[0]
             if base_url == "https:/": # linked to an anchor of a base page
                 return None
         full_link = base_url + "/" + no_anchor
-    print("finished cleaning, link is", full_link)
 
     return full_link
 
@@ -52,9 +49,29 @@ def clean_links(base_url, dirty_links):
             continue
         if has_linked.get(cleaned, False):
             continue
+        if not has_html(cleaned):
+            continue
         full_links.append(cleaned)
         has_linked[cleaned] = True
     return full_links
+
+def has_html(url):
+    """determines if a url ends with anything other than a directory, .html,
+    .xhtml, or .php, requires a full url"""
+    print("checking", url)
+    good_filetypes = [".html", ".xhtml", ".php"]
+    pattern = re.compile(r'\.\w+')
+    tld_split = url.split(get_tld(url))
+    print(tld_split)
+    if len(tld_split) > 1 and len((tld_split)[1]) > 1:
+        after_tld = tld_split[1]
+        extension = re.findall(pattern, after_tld)
+        print(extension)
+        if not extension:
+            return True
+        if extension[-1] not in good_filetypes:
+            return False
+    return True
 
 have_visited = {}
 
@@ -71,15 +88,15 @@ def all_links(input_url):
 
     full_links = clean_links(input_url, links)
     for link in full_links:
-        #print("found", link)
+        print("found", link)
         if have_visited.get(link, False):
             print("but we already visited it!")
         else:
             have_visited[link] = True
-            #all_links(link)
+            all_links(link)
 
     return full_links
 
-all_links("https://help.github.com/enterprise/2.7/user")
+#all_links("https://help.github.com/enterprise/2.7/user")
 #url = "https://github.com/rivergillis/crawler/blob/master/crawler.py"
 #all_links(url)
