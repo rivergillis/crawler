c147110b99cd196d49f7d445b8dc3d9507d9965f
River Gillis <jrivergillis@gmail.com>
2016-11-05 22:45:03 -0500

    Fix bug when crawling a link to /

--- crawler.py
+++ crawler.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup, SoupStrainer
 import requests
 import re
 
-def get_root_domain(link):
+def get_root_url(link):
     """Given a url in the form https://help.github.com/enterprise/2.7/user/, returns https://help.github.com/
     """
     if not link[-1] == '/':
@@ -17,7 +17,7 @@ def get_root_domain(link):
 def remove_anchor(link):
     return link.split("#")[0]
 
-def clean_link(base_url, dirty_link):
+def clean_link(base_url, dirty_link, root_url):
     print("cleaning link", dirty_link, "with base", base_url)
     no_anchor = remove_anchor(dirty_link)
     if no_anchor.startswith('http://') or no_anchor.startswith('https://'):
@@ -25,13 +25,15 @@ def clean_link(base_url, dirty_link):
     else:
         if base_url.endswith("/"):
             base_url = base_url[:-1]
-        if no_anchor.endswith("/"): #trailing slash
+        if no_anchor.endswith("/") and len(no_anchor) > 1: #trailing slash
             no_anchor = no_anchor[:-1]
         if no_anchor.startswith("../"):
             no_anchor = no_anchor[3:]
             base_url = base_url.rsplit("/", 2)[0]
         elif no_anchor.startswith("/"): # root + extra
-            full_url = base_url + no_anchor
+            print("found a root call, root is", root_url)
+            print("returning", root_url + no_anchor[1:])
+            return root_url + no_anchor[1:]
         else:
             base_url = base_url.rsplit("/", 1)[0]
             if base_url == "https:/": # linked to an anchor of a base page
@@ -45,7 +47,7 @@ def clean_links(base_url, dirty_links):
     has_linked = {}
     full_links = []
     for link in dirty_links:
-        cleaned = clean_link(base_url, link)
+        cleaned = clean_link(base_url, link, get_root_url(base_url))
         if not cleaned:
             continue
         if has_linked.get(cleaned, False):
@@ -69,7 +71,7 @@ def all_links(input_url):
 
     full_links = clean_links(input_url, links)
     for link in full_links:
-        print("found", link)
+        #print("found", link)
         if have_visited.get(link, False):
             print("but we already visited it!")
         else:
@@ -78,6 +80,6 @@ def all_links(input_url):
 
     return full_links
 
-#all_links("https://help.github.com/enterprise/2.7/user")
+all_links("https://help.github.com/enterprise/2.7/user")
 #url = "https://github.com/rivergillis/crawler/blob/master/crawler.py"
 #all_links(url)
