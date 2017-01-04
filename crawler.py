e399d0c17e8fc9fa493bfd442fe7703e2a45613f
River <jrivergillis@gmail.com>
2017-01-04 16:39:26 -0600

    Pages are now tracked and skipped correctly

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup, SoupStrainer
 from link import Link
+from page import Page
 import requests
 
 # TODO: cover all cases where a url is empty
@@ -18,18 +19,70 @@ def move_up_dirs(link, root_url):
     """
     pass
 
-have_visited = set()
-depth = 0
+grabbed_links = []  # Grabbed links contains the list of upcoming Links to be crawled
+has_grabbed = set()  # has_grabbed contains a set of Links that we have already grabbed
+page_counts = {}   # page_counts contains the number of times a certain page has been grabbed (Page -> Count)
+pages_by_links = {}  # pages_by_links has a page object for a corresponding link object (Link -> Page)
 
 
-def all_links(input_url):
+def begin_crawl(input_url):
     """
+    calls crawl and maintains calling it in a while loop, keeping track of the # of links grabbed
+    :param input_url: the first link to begin crawling on, a string
+    :return: the length of the crawl, the number of links grabbed (that were unique on a per-page basis)
+    """
+    crawl_length = 0
+    grabbed_links.append(Link(input_url, "http://www.rivergillis.com/"))
+    while grabbed_links:
+        crawl()
+        crawl_length += 1
+        print("crawl length is now", crawl_length)
+
+    return crawl_length
+
+
+def crawl():
+    """
+    pops off the top link from grabbed_links, makes a page for it, updates the page counts and extends grabbed_links
+    :return: length of the crawl
+    """
+    if not grabbed_links:
+        return
+    current_link = grabbed_links.pop()
+
+    if current_link in has_grabbed:
+        # If we've already grabbed this link before, increment the corresponding page and return
+        page_to_increment = pages_by_links[current_link]
+        page_counts[page_to_increment] += 1
+        print("we've been to", current_link, page_counts[page_to_increment], "times now!")
+        return
+    else:
+        has_grabbed.add(current_link)
+
+    print("-----VISITING", current_link, "------")
+
+    # Create a Page from the Link and connect the Link to the Page via pages_by_links
+    current_page = Page(current_link.full_hyperlink)
+    pages_by_links[current_link] = current_page
+
+    # Updates the page_counts
+    page_counts[current_page] = 1
+
+    # updates the grabbed_links
+    grabbed_links.extend(current_page.links)
+    print("This page has", len(current_page.links), "links, we've now got", len(grabbed_links), "total links")
+    return
+
+
+"""
+def _crawl(input_url):
+
     input_url: a string of a url in a fully accessible form
     Downloads the web page attached to the string and searches
     for any links contained within. Visits each web page linked
     recursively and eventually returns a list of every link found
     on that page
-    """
+
     
     # TODO: a valid html link begins with <!DOCTYPE html>
     # TODO: check for if we've visited https or http version (also maybe check for www or not)
@@ -60,12 +113,13 @@ def all_links(input_url):
             continue
         have_visited.add(link)
         print("found new: ", link)
-        all_links(link.full_hyperlink)
+        crawl(link.full_hyperlink)
 
     print("now returning")
     depth -= 1
     return link_objects
+"""
 
 if __name__ == "__main__":
-    url = "http://miniorange.com/fraud/"
-    all_links(url)
+    url = "https://reddit.com/"
+    begin_crawl(url)
